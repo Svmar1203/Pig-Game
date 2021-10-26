ad46da391ad2e80af0527809609c6b816eb8a98d
Svmar1203 <83650741+Svmar1203@users.noreply.github.com>
2021-10-27 00:00:35 +0300

    added changes

--- Game-Roll-Dice/script.js
+++ Game-Roll-Dice/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Selecting elements
-/*
+
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
@@ -14,12 +14,9 @@ const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 
 // Starting conditions
-score0El.textContent = 0;
 score1El.textContent = 0;
-diceEl.classList.add('hidden');
-let currentScore = 0;
-const scores = [0, 0];
-let activePlayer = 0;
+score0El.textContent = 0;
+
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
@@ -27,7 +24,28 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
-let playing = true;
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  diceEl.classList.add('hidden');
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -50,42 +68,22 @@ btnRoll.addEventListener('click', function () {
 
 btnHold.addEventListener('click', function () {
   if (playing) {
-    // 1.Add current score to active player's score
+    diceEl.classList.add('hidden');
     scores[activePlayer] += currentScore;
-    console.log(scores[activePlayer]);
-    //scores[1] = scores[1] + currentScore
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
-    // 2.Check if player's score is >= 100
     if (scores[activePlayer] >= 50) {
       playing = false;
-      diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove('player-active');
+        .classList.remove('player--active');
     } else {
-      // Switch to next player
       switchPlayer();
     }
   }
 });
-*/
 
-const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
-const btnRoll = document.querySelector('.btn--roll');
-const btnNew = document.querySelector('.btn--new');
-const btnHold = document.querySelector('.btn--hold');
-
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
-btnRoll.addEventListener('click', function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  diceEl.classList.remove('hidden');
-});
+btnNew.addEventListener('click', init);
